2cab8ce0c24025babe2aed68e75ac1fdb4001697
Quan Vo Van <44120924+quanvovan@users.noreply.github.com>
2019-11-17 22:04:41 +0700

    update test.c

--- random/test.c
+++ random/test.c
@@ -1,43 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <errno.h>
-#include <string.h>
 #include <unistd.h>
-#include <string.h>
-
-char Random[256];
 
 int main() {
     int ret, fd;
+    unsigned int random;
     printf("Starting device test code example...\n");
-    printf(".\n.\n.\n");
+    printf("...\n...");
 
-    while (1) {
-        printf("Press ENTER to read from the device...");
-        fd = open("/dev/random", O_RDWR);  // Open the device with read/write access
-        if (fd < 0){
-            perror("Failed to open the device!");
-            return errno;
-        }
-        char c = getchar();
-
-        if (c != 10) {
-            close(fd);
-            break;
-        }
+    fd = open("/dev/randomDevice", O_RDWR); 
+    //fd = open("/dev/randomDevice", O_RDWR);  // Open the device with read/write access
+    if (fd < 0){
+          printf("Failed to open the device!");
+          return -1;
+     }
+       
 
         printf("Reading from the device...\n");
-        ret = read(fd, Random, 256);        // Read the response from the LKM
+        ret = read(fd, &random, sizeof(unsigned int));
+        //ret = read(fd, random, sizeof(unsigned int));        // Read the response from the LKM
         if (ret < 0){
             close(fd);
-            perror("Failed to read the message from the device!");
-            return errno;
+            printf("Failed to read the message from the device!");
+            return -1;
         }
 
-        printf("The received random number is: [%s]\n\n", Random);
+        printf("Gia tri random thu duoc la: %u\n", random);
         close(fd);
-    }
+    
 
     printf("End of the program\n");
     return 0;
